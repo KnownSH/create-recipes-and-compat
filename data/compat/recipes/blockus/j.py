91012886bd79b99978386757ccda05f4ebc59bc6
KnownSH <71918238+KnownSH@users.noreply.github.com>
2023-07-01 21:19:23 -0700

    changed formatting

--- data/compat/recipes/blockus/j.py
+++ data/compat/recipes/blockus/j.py
@@ -8,8 +8,15 @@ for filename in os.listdir(directory):
     item = ""
 
     if os.path.isfile(filedir):
-        with open(filedir, 'r') as f:
+        with open(filedir, 'r+') as f:
             data = json.load(f)
             item = data['results'][0]['item']
+            if item.endswith("slab"):
+                data['results'][0]["count"] = 2
+            else:
+                data['results'][0]["count"] = 1
             r, item = item.rsplit(':', 1)
+            f.seek(0)
+            json.dump(data, f, indent=4)
+            f.truncate()
         os.rename(filedir, '{}{}.json'.format(directory, item))
